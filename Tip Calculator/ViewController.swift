ef3d568fbe00e443c5fe9593e359c88a0cacee2e
Bobby <bfrydell@gmail.com>
2016-12-15 19:43:50 -0500

    added faulty computeTip method/function/whatever

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func computeTip(_ sender: Any) {
+        let price = Double(priceTextField.text!)
+        let tip = price! * 1.2
+        tipLabel.text = "Your 20% tip is \(tip)"
+        tipLabel.isHidden = false
+    }
 
 }
 
